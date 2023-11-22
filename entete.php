f9ff2f9b9390a37ad252618cac55507d5ec94abf
AnissaBS <139979527+AnissaBS@users.noreply.github.com>
2023-11-22 16:53:06 +0100

    Add files via upload

---
+++ entete.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Nanum+Myeongjo&family=Playfair+Display&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="entete_style.css">
+</head>
+<body>
+
+  <header>
+    <h2 id="titre"><a href="index.php">CaninCâlin</a></h2>
+
+    <nav>
+      <ul>
+        <li><a href="index.php">Accueil</a></li>
+        <li>
+          <a href="#">Qui sommes-nous ?</a>
+          <ul>
+            <li><a href="#">Nos missions</a></li>
+            <li><a href="#">Notre équipe</a></li>
+          </ul>
+        </li>
+        <li>
+          <a href="#">Nos services</a>
+          <ul>
+            <li><a href="#">Consultations</a></li>
+            <li><a href="#">Vaccination</a></li>
+            <li><a href="#">Chirurgie</a></li>
+            <li><a href="#">Hospitalisation</a></li>
+            <li><a href="#">Imagerie</a></li>
+            <li><a href="#">Vente de médicaments</a></li>
+          </ul>
+        </li>
+        <li><a href="contact.php">Nous contacter</a></li>
+      </ul>
+    </nav>
+
+    <div class="recherche">
+      <input type="text" placeholder="Rechercher...">
+      <input type="submit" value="Entrer">
+    </div>
+  </header>
+
+</body>
+</html>

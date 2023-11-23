3b916870c820a0b5f43605cec85d2eb9f21de28a
AnissaBS <139979527+AnissaBS@users.noreply.github.com>
2023-11-23 08:47:52 +0100

    Add files via upload

---
+++ foot.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="foot_style.css">
+    <title>Document</title>
+</head>
+<body>
+        
+<footer>
+    <div class="contenu-footer">
+        <div class="bloc footer-services">
+            <h3>Nos services</h3>
+            <ul class="liste-services">
+                <li><a href="#">Consultations</a></li>
+                <li><a href="#">Vaccination</a></li>
+                <li><a href="#">Chirurgie</a></li>
+                <li><a href="#">Hospitalisation</a></li>
+                <li><a href="#">Imagerie</a></li>
+                <li><a href="#">Ventes</a></li>
+            </ul>
+        </div>
+        <div class="bloc footer-contact">
+            <h3>Restons en contact</h3>
+            <p>Téléphone : 07-08-09-10-11</p>
+            <p>Email : gveter@gmail.com</p>
+            <p>Adresse : 28, avenue de l'Abbé Pierre, 45000 Orléans</p>
+        </div>
+        <div class="bloc footer-horaires">
+            <h3>Nos horaires</h3>
+            <ul class="liste-horaires">
+                <li>✔️&emsp; Lun 9h - 18h</li>
+                <li>✔️&emsp; Mar 9h - 18h</li>
+                <li>✔️&emsp; Mer 10h - 18h</li>
+                <li>✔️&emsp; Jeu 9h - 18h</li>
+                <li>✔️&emsp; Ven 9h - 18h</li>
+                <li>✔️&emsp; Sam 10h - 15h</li>
+                <li>❌&emsp; Dim Fermé</li>
+                <li>&emsp;</li>
+                <li class="garde">N'hésitez pas à nous contacter pour obtenir davantage d'informations concernant les heures et lieux de garde.</li>
+            </ul>
+        </div>
+        <div class="bloc footer-medias">
+            <h3>Nos réseaux sociaux</h3>
+            <ul class="liste-medias">
+                <li><a href="#"><img src="facebook.png" alt="">&ensp; Facebook</a></li>
+                <li><a href="#"><img src="instagram_2111463.png" alt="">&ensp; Instagram</a></li>
+                <li><a href="#"><img src="twitter_4494477.png" alt="">&ensp; Twitter</a></li>
+                <li><a href="#"><img src="youtube_4494485.png" alt="">&ensp; YouTube</a></li>
+            </ul>
+        </div>
+    </div>
+    </footer>
+    
+    </body>
+    </html>
+</body>
+</html>

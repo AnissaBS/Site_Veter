fe7cb8582476be611bcfdfc2386c65a80a9fde9d
AnissaBS <139979527+AnissaBS@users.noreply.github.com>
2023-11-29 16:36:30 +0100

    Add files via upload

--- entete.php
+++ entete.php
@@ -3,49 +3,52 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <!-- <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Nanum+Myeongjo&family=Playfair+Display&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Nanum+Myeongjo&family=Playfair+Display&display=swap" rel="stylesheet"> -->
   <link rel="stylesheet" href="entete_style.css">
 </head>
 <body>
 
   <header>
-  <div>
+    <div>
       <a href="index.php"><img src="Ressources/logo.png" alt=""  id="logo"></a>
     </div>
 
     <nav>
       <div id="menu">
         <ul>
-          <li><a href="index.php">Accueil</a></li>
+          <li><a href="index.php" id="entt1">Accueil</a></li>
           <li>
-            <a href="#">Qui sommes-nous ?</a>
+            <a href="#" id="entt2">Qui sommes-nous ?</a>
             <ul>
+              <li><a href="#">Notre histoire</a></li>
               <li><a href="#">Nos missions</a></li>
               <li><a href="#">Notre équipe</a></li>
+              <li><a href="#">Nos bénévoles</a></li>
             </ul>
           </li>
           <li>
-            <a href="#">Nos services</a>
+            <a href="#" id="entt3">Nos services</a>
             <ul>
               <li><a href="#">Consultations</a></li>
               <li><a href="#">Vaccination</a></li>
               <li><a href="#">Chirurgie</a></li>
               <li><a href="#">Hospitalisation</a></li>
-              <li><a href="#">Imagerie</a></li>
+              <li><a href="#">Imagerie Médicale</a></li>
               <li><a href="#">Vente de médicaments</a></li>
             </ul>
           </li>
-          <li><a href="contact.php">Nous contacter</a></li>
+          <li><a href="contact.php" id="entt4">Infos pratiques</a></li>
+          <li><a href="contact.php" id="entt5">Nous contacter</a></li>
         </ul>
       </div>
     </nav>
 
-    <div class="recherche">
+    <!-- <div class="recherche">
       <input type="text" placeholder="Rechercher...">
       <input type="submit" value="Entrer">
-    </div>
+    </div> -->
   </header>
 
 </body>

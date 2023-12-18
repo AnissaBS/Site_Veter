20eb67117a3d237453762f3f904e7e0a439df2ce
AnissaBS <139979527+AnissaBS@users.noreply.github.com>
2023-12-18 16:42:14 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -9,7 +9,6 @@
 <body>
 
 <div class="surback">    
-
     <div class="contenu">
 
         <div class="video">
@@ -24,11 +23,31 @@
             <div id="CC">
                 <h1>CaninCâlin</h1>
                 <p>Bienvenue chez votre vétérinaire préféré.</p>
-                <a href="#categ" class="button">En savoir plus</a>
+                <a href="#serv" class="button">En savoir plus</a>
             </div>
         </div>
 
-        <div class="pp" id="categ">
+    <div class="QUI" id="serv">
+        <table>
+            <tbody>
+            <label for="">Qui sommes-nous ?</label>
+                <tr>
+                    <td><a href="#pageHist">Notre histoire</a></td>
+                    <td rowspan=2><a href="#logo" id="btnImg"><img src="Ressources/logo.png"></a></td>
+                    <td><a href="#pageEquip">Notre équipe</a></td>
+                </tr>
+                <tr>
+                    <td><a href="#pageMiss">Nos missions</a></td>
+                    <td><a href="#pageBenev">Nos bénévoles</a></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    </div>
+</div>
+
+        <!-- <div class="pp" id="categ">
             <div class="pp-equipe">
                 <h1>Qui est G_veter ?</h1>
                 <a href="#"><img src="equipe.jpg" alt=""></a>
@@ -54,9 +73,7 @@
                 <h1>Contactez-nous</h1>
                 <a href="contact.html" class="btn-contact">Accès au formulaire de contact</a>
             </div>
-        </div>
-    </div>
-</div>
+        </div> -->
 
 <?php
 include_once('foot.php');
